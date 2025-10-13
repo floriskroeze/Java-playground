39aeb6ec4c35532f187d34e25d219e2d0c8dc346
Floris Kroeze <floriskr@gmail.com>
2025-10-13 12:18:41 +0200

    Encapsulation

--- OOPFundamentals/src/Main.java
+++ OOPFundamentals/src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("OOP is cool!");
+        Car audi = new Car();
+
+        audi.setMake("Audi");
+        audi.setModel("Q5");
+        audi.setColor("Black");
+        audi.setDoors(5);
+        audi.setConvertible(false);
+
+        audi.describeCar();
+
+        Car tesla = new Car();
+        tesla.setMake("Tesla");
+        tesla.setModel("Model Y");
+        tesla.setColor("Blue");
+        tesla.setDoors(5);
+        tesla.setConvertible(true);
+
+        tesla.describeCar();
     }
 }
