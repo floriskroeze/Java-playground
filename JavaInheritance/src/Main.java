951ff07d68b007ccfccd18b1caf06548db4768ff
Floris Kroeze <floriskr@gmail.com>
2025-10-18 10:59:01 +0200

    Java inheritance

---
+++ JavaInheritance/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal("Generic animal", "Huge", 400);
+        doAnimalStuff(animal, "slow");
+
+        Dog yorkie = new Dog("Yorkie", 15);
+        doAnimalStuff(yorkie, "fast");
+
+        Dog retriever = new Dog("Labrador retriever", 65, "Floppy", "Swimmer");
+        doAnimalStuff(retriever, "slow");
+    }
+
+    public static void doAnimalStuff(Animal animal, String speed) {
+        animal.makeNoise();
+        animal.move(speed);
+        System.out.println(animal);
+        System.out.println("_ _ _ _");
+    }
+}
