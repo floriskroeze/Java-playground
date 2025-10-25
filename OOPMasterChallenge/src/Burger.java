4d9898b4735584a81dee79c98cd89e7be025a871
Floris Kroeze <floriskr@gmail.com>
2025-10-25 17:23:22 +0200

    OOP Master Challenge

---
+++ OOPMasterChallenge/src/Burger.java
@@ -0,0 +1,13 @@
+public class Burger extends Item {
+    private String firstTopping;
+    private String secondTopping;
+    private String thirdTopping;
+
+    public Burger(String name, String type, int price) {
+        super(name, type, price);
+    }
+
+    public void addToppings(String topping) {
+
+    }
+}
