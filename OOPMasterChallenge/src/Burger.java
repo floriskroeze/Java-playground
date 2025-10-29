867ec6ac6aa87185d55cc55d04643c987133e727
Floris Kroeze <floriskr@gmail.com>
2025-10-29 11:53:43 +0100

    OOP Master Challenge

--- OOPMasterChallenge/src/Burger.java
+++ OOPMasterChallenge/src/Burger.java
@@ -1,13 +1,88 @@
 public class Burger extends Item {
-    private String firstTopping;
-    private String secondTopping;
-    private String thirdTopping;
+    private Item extra1;
+    private Item extra2;
+    private Item extra3;
 
-    public Burger(String name, String type, int price) {
-        super(name, type, price);
+    public Burger(String name, double price) {
+        super("Burger",name , price);
     }
 
-    public void addToppings(String topping) {
+    @Override
+    public String getName() {
+        return super.getName() + " BURGER";
+    }
 
+    @Override
+    public double getAdjustedPrice() {
+        return getBasePrice() +
+                ((extra1 == null) ? 0 : extra1.getAdjustedPrice()) +
+                ((extra2 == null ) ? 0 : extra2.getAdjustedPrice()) +
+                ((extra3 == null ) ? 0 : extra3.getAdjustedPrice());
+    }
+
+    public double getExtraPrice(String toppingName) {
+        return switch (toppingName.toUpperCase()) {
+            case "AVOCADO", "CHEESE" -> 1.0;
+            case "BACON", "HAM", "SALAMI" -> 1.5;
+            default -> 0.0;
+        };
+    }
+
+    public void addToppings(String extra1, String extra2, String extra3) {
+        this.extra1 = new Item("TOPPING", extra1, getExtraPrice(extra1));
+        this.extra2 = new Item("TOPPING", extra2, getExtraPrice(extra2));
+        this.extra3 = new Item("TOPPING", extra3, getExtraPrice(extra3));
+    }
+
+    public void printItemizedList() {
+        printItem("BASE BURGER", getBasePrice());
+        if (extra1 != null) {
+            extra1.printItem();
+        }
+        if (extra2 != null) {
+            extra2.printItem();
+        }
+        if (extra3 != null) {
+            extra3.printItem();
+        }
+    }
+
+    @Override
+    public void printItem() {
+
+        printItemizedList();
+        System.out.println("-".repeat(30));
+        super.printItem();
+    }
+}
+
+class DeluxeBurger extends Burger {
+    private Item deluxe1;
+    private Item deluxe2;
+
+    public DeluxeBurger(String name, double price) {
+        super(name, price);
+    }
+
+    public void addToppings(String extra1, String extra2, String extra3, String extra4, String extra5) {
+        super.addToppings(extra1, extra2, extra3);
+        deluxe1 = new Item("TOPPING", extra4, 0);
+        deluxe2 = new Item("TOPPING", extra5, 0);
+    }
+
+    @Override
+    public void printItemizedList() {
+        super.printItemizedList();
+        if(deluxe1 != null) {
+            deluxe1.printItem();
+        }
+        if(deluxe2 != null) {
+            deluxe2.printItem();
+        }
+    }
+
+    @Override
+    public double getExtraPrice(String toppingName) {
+        return 0;
     }
 }
--- OOPMasterChallenge/src/Main.java
+++ OOPMasterChallenge/src/Main.java
@@ -1,6 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        MealOrder mealOrder = new MealOrder();
-        mealOrder.order();
+//        Item coke = new Item("drink", "coke", 1.50);
+//        coke.printItem();
+//        coke.setSize("LARGE");
+//        coke.printItem();
+//
+//        Item avocado = new Item("Topping", "avocado", 1.50);
+//        avocado.printItem();
+//
+//        Burger burger = new Burger("Regular", 4.00);
+//        burger.addToppings("BACON", "CHEESE", "MAYO");
+//        burger.printItem();
+
+//        MealOrder mealOrder = new MealOrder();
+//        mealOrder.setDrinkSize("LARGE");
+//        mealOrder.addBurgerToppings("BACON", "CHEESE", "MAYO");
+//        mealOrder.printItemizedList();
+
+        MealOrder secondMeal = new MealOrder("deluxe", "7-up", "chili");
+        secondMeal.addBurgerToppings("LETTUCE", "CHEESE", "MAYO", "KETCHUP", "ONIONRINGS");
+        secondMeal.setDrinkSize("SMALL");
+        secondMeal.printItemizedList();
+
     }
 }
